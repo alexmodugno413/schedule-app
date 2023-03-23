8bc2886ddcd0b77b904009c2e0196a983baf1990
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-03-23 12:28:13 -0500

    Implement selecting courses

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,13 +2,22 @@ import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.css';
 import { ListGroup } from 'react-bootstrap';
 import './CourseList.css';
+import { useState } from 'react';
 
 const CourseList = ({courses, term}) => {
+    const [toggled, setToggled] = useState([]);
+
+    const onClickFunction = (id) => {
+        toggled.includes(id) ? setToggled(toggled.filter(x => x !== id)) : setToggled([...toggled, id]);
+    }
+
     const newCourses = Object.entries(courses).filter(([id, info]) => info.term === term);
+    console.log(newCourses);
     return (
         <div className="course-list">
         { Object.entries(newCourses).map(([id, info]) =>
-            <Card style={{ width: '13.5rem' }}>
+            <Card style={{ width: '13.5rem' }} bg={toggled.includes(id) ? 'success' : ''}>
+                <button onClick={() => onClickFunction(id)}>Select Class</button>
                 <Card.Body>
                     <Card.Title>{info[1].term} CS {info[1].number}</Card.Title>
                     <Card.Text>{info[1].title}</Card.Text>
